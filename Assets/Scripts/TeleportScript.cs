12ac4432533c798b74c1793d7e7dbc8a6c70dd21
SuperEvangelista1 <jhojanevangelista.sia@gmail.com>
2025-01-06 18:37:51 -0500

    Basic barebones map and all warp points added
    basic map layout and warp/anchor points have been added until further notice to replace them comes lol

--- Assets/Scripts/TeleportScript.cs
+++ Assets/Scripts/TeleportScript.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class TeleportScript : MonoBehaviour
 {
+    public GameObject anchorpoint1;
     public GameObject anchorpoint2;
-    
+    public GameObject anchorpoint3;
+    public GameObject anchorpoint4;
+    public GameObject anchorpoint5;
+    public GameObject anchorpoint6;
+    public GameObject anchorpoint7;
+    public GameObject anchorpoint8;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +28,36 @@ public class TeleportScript : MonoBehaviour
     {
        if(collision.gameObject.tag == "AnchorStart")
        {
-            transform.position = anchorpoint2.gameObject.transform.position;
+            transform.position = anchorpoint1.gameObject.transform.position;
        }
+        if (collision.gameObject.tag == "AnchorStart2")
+        {
+            transform.position = anchorpoint2.gameObject.transform.position;
+
+        }
+        if (collision.gameObject.tag == "AnchorStart3")
+        {
+            transform.position = anchorpoint3.gameObject.transform.position;
+        }
+        if (collision.gameObject.tag == "AnchorStart4")
+        {
+            transform.position = anchorpoint4.gameObject.transform.position;
+        }
+        if (collision.gameObject.tag == "AnchorStart5")
+        {
+            transform.position = anchorpoint5.gameObject.transform.position;
+        }
+        if (collision.gameObject.tag == "AnchorStart6")
+        {
+            transform.position = anchorpoint6.gameObject.transform.position;
+        }
+        if (collision.gameObject.tag == "AnchorStart7")
+        {
+            transform.position = anchorpoint7.gameObject.transform.position;
+        }
+        if (collision.gameObject.tag == "AnchorStart8")
+        {
+            transform.position = anchorpoint8.gameObject.transform.position;
+        }
     }
 }
