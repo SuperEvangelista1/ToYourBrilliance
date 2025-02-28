6eadb3b237159c4647605939f9e32cc7ad2646e4
Yuanyuan-07 <yuanyuanzeng.sia@gmail.com>
2025-02-28 16:45:47 -0500

    Inventory

--- Assets/Scripts/InventoryScript/PlayerInventory.cs
+++ Assets/Scripts/InventoryScript/PlayerInventory.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
+using TMPro; // Import for TextMeshPro support
 
 public class PlayerInventory : MonoBehaviour
 {
     public List<ObjectValue> inventory = new List<ObjectValue>();
+    public TextMeshProUGUI inventoryText; // Reference to the UI text
 
     // Method to handle when the player picks up an object
     public void AddObject(string objectName)
@@ -22,6 +25,20 @@ public class PlayerInventory : MonoBehaviour
             // If the object doesn't exist, create a new ObjectValue and add it to the list
             ObjectValue newObject = new ObjectValue(objectName);
             inventory.Add(newObject);
+        }
+
+        // Update the inventory UI
+        UpdateInventoryUI();
+    }
+
+    // Method to update the UI text
+    void UpdateInventoryUI()
+    {
+        inventoryText.text = "Inventory:\n";
+
+        foreach (ObjectValue obj in inventory)
+        {
+            inventoryText.text += obj.objectName + " x" + obj.value + "\n";
         }
     }
 }
